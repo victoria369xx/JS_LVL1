f70cdf32a31b17bc390e26112cf7cfebbab43f6b
Victoria <v.shurkalina@gmail.com>
2021-05-31 19:20:13 +0400

    homework2

---
+++ hw2/task4.js
@@ -0,0 +1,52 @@
+
+        /** 
+         * Функция складывает два числа.
+         * @param {number} num1  функция принимает первое число.
+         * @param {number} num2 функция принимает второе число.
+         * @returns {number} функция возвращает результат сложения.
+         */
+         function sum (num1, num2) {
+            return num1 + num2;
+             
+         }
+         let result1 = sum (4,5); 
+         alert (result1);
+
+          /** 
+         * Функция выполняет вычитание двух чисел.
+         * @param {number} num1  функция принимает первое число.
+         * @param {number} num2 функция принимает второе число.
+         * @returns {number} функция возвращает разность чисел.
+         */
+           function sub (num1, num2) {
+            return num1 - num2;
+             
+         }
+         let result2 = sub (7,5); 
+         alert (result2);
+         
+           /** 
+         * Функция находит произведение двух чисел.
+         * @param {number} num1  функция принимает первое число.
+         * @param {number} num2 функция принимает второе число.
+         * @returns {number} функция возвращает произведение двух чисел.
+         */
+            function mul (num1, num2) {
+                return num1 * num2;
+                 
+             }
+             let result3 = mul (3,4); 
+             alert (result3);
+
+           /** 
+         * Функция выполняет деление двух чисел.
+         * @param {number} num1  функция принимает первое число.
+         * @param {number} num2 функция принимает второе число.
+         * @returns {number} функция возвращает частное чисел.
+         */
+            function div (num1, num2) {
+                return num1 / num2;
+                 
+             }
+             let result4 = div (16,2); 
+             alert (result4);
